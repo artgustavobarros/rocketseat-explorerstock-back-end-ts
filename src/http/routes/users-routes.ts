8effcb6370d0b8956f7fdd7b819605429fbb22e0
Arthur Barros <135854588+th3gust@users.noreply.github.com>
2024-04-28 16:53:13 -0300

    04.28

--- src/http/routes/users-routes.ts
+++ src/http/routes/users-routes.ts
@@ -3,6 +3,6 @@ import { registerUser } from '../controllers/users/register'
 import { sessionsUser } from '../controllers/users/sessions'
 
 export const usersRoutes = async (app: FastifyInstance) => {
-  app.post('/register', registerUser)
-  app.post('/session', sessionsUser)
+  app.post('/users', registerUser)
+  app.post('/sessions', sessionsUser)
 }
